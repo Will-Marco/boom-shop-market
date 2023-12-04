f91159c41c2b8ceadd2be753b7f46c7a5d9e9f1f
Will-Marco <mr.marco0101@gmail.com>
2023-12-04 21:57:15 +0500

    add dinamic title

--- routes/auth.js
+++ routes/auth.js
@@ -3,11 +3,17 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/login", (req, res) => {
-  res.render("login");
+  res.render("login", {
+    title: "Login",
+    isLoginPage: true,
+  });
 });
 
 router.get("/register", (req, res) => {
-  res.render("register");
+  res.render("register", {
+    title: "Registerations",
+    isRegisterPage: true,
+  });
 });
 
 export default router;
--- routes/products.js
+++ routes/products.js
@@ -3,15 +3,23 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", {
+    title: "Boom | Shop",
+  });
 });
 
 router.get("/products", (req, res) => {
-  res.render("products");
+  res.render("products", {
+    title: "Products",
+    isProductsPage: true,
+  });
 });
 
 router.get("/add", (req, res) => {
-  res.render("add");
+  res.render("add", {
+    title: "Add product",
+    isAddPage: true,
+  });
 });
 
 export default router;
