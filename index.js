5fcc0ae9d420b6663033fddf4b17becf0f4d715b
Will-Marco <mr.marco0101@gmail.com>
2023-12-06 22:54:17 +0500

    add database connection

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
-import express, { static as static_, urlencoded } from "express";
+import express, { json, static as static_, urlencoded } from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 import { create } from "express-handlebars";
+// ROUTES
 import AuthRoutes from "./routes/auth.js";
 import ProductsRoutes from "./routes/products.js";
 
-const app = express();
 dotenv.config();
 
-const hbs = create({
-  defaultLayout: "main",
-  extname: "hbs",
-});
+const app = express();
+
+const hbs = create({ defaultLayout: "main", extname: "hbs" });
 app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 app.set("views", "./views");
+
 app.use(static_("public"));
 app.use(urlencoded({ extended: true }));
+app.use(json());
 
 app.use(AuthRoutes);
 app.use(ProductsRoutes);
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log("Server has been started on port " + port);
-});
+const startApp = () => {
+  try {
+    mongoose.set("strictQuery", false);
+    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }, () =>
+      console.log("Mongo DB connected")
+    );
+    const PORT = process.env.PORT || 4100;
+    app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startApp();
